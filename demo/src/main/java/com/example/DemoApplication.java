16ffd3a1e369e971cb39317eaf00d27fabac702e
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-13 12:17:37 +0200

    basico

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.ioc.NotificationService;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -16,4 +20,17 @@ public class DemoApplication implements CommandLineRunner {
 		System.err.println("Apicacion arrancada....");
 	}
 
+	@Autowired
+	NotificationService notify;
+	
+	@Bean
+	CommandLineRunner ioc() {
+		return args -> {
+			notify.add("Mensaje 1");
+			notify.add("Mensaje 2");
+			notify.add("Mensaje 3");
+			notify.getListado().forEach(System.out::println);
+			System.out.println(notify.getClass().getCanonicalName());
+		};
+	}
 }
