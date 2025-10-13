d3ec8e2b1e159ac08ba4602210de4dd85f3e572e
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-13 12:36:31 +0200

    cadenaDeDependencia

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.example.ioc.NotificationService;
+import com.example.ioc.contratos.Configuracion;
+import com.example.ioc.contratos.ServicioCadenas;
+import com.example.ioc.implementaciones.ConfiguracionImpl;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -23,14 +26,33 @@ public class DemoApplication implements CommandLineRunner {
 	@Autowired
 	NotificationService notify;
 	
-	@Bean
-	CommandLineRunner ioc() {
+//	@Bean
+	CommandLineRunner ioc(Configuracion config) {
 		return args -> {
+//			System.out.println(notify.getClass().getCanonicalName());
 			notify.add("Mensaje 1");
 			notify.add("Mensaje 2");
 			notify.add("Mensaje 3");
+			config.config();
+			Configuracion c = new ConfiguracionImpl(notify);
+			c.config();
 			notify.getListado().forEach(System.out::println);
 			System.out.println(notify.getClass().getCanonicalName());
+		};
+	}
+	
+	@Bean
+	CommandLineRunner cadenaDeDependencia(ServicioCadenas srv) {
+		return args -> {
+			srv.get().forEach(notify::add);
+			srv.add("Hola mundo");
+			notify.add(srv.get(1));
+			srv.modify("modificado");
+			System.out.println("===================>");
+			notify.getListado().forEach(System.out::println);
+			notify.clear();
+			System.out.println("<===================");
+			
 		};
 	}
 }
